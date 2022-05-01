be2572872ad2cf7f9dd58466893672faad2577bf
Andrej Šípka <andrej@Andrejs-MacBook-Pro.local>
2022-05-01 12:03:51 +0200

    init code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
+  const [timer, setTimer] = useState(false);
+
+  const resetTimer = () => {
+    setMinutes('');
+    setSeconds('');
+  }
+
+  const Countdown = () => {
+    useEffect(() => {
+      const counter = setInterval(() => {
+        if( seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        
+        if(seconds === 0) {
+          if(minutes === 0) {
+            // Reset the timer
+            clearInterval(counter);
+            resetTimer()
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => {
+        clearInterval(counter);
+      }
+    });
+    return (
+      <div>
+        <p>{minutes}:{seconds}</p>
+      </div>
+    )
+  }
+
+  const startTimer = (e) => {
+    e.preventDefault();
+
+    const time = {
+      minute: minutes,
+      second: seconds,
+    }
+
+    setTimer(true);
+    console.log('Timer started', time);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Quick Timer with React</h1>
+        <form onSubmit={startTimer}>
+          <label>
+            <span>Minute</span>
+            <input type="number" id="minute" name="minute" placeholder="0" onChange={(e) => setMinutes(e.target.value)} value={minutes} />
+          </label>
+
+          <label>
+            <span>Second</span>
+            <input type="number" id="second" name="second" placeholder="0" onChange={(e) => setSeconds(e.target.value)} value={seconds} />
+          </label>
+          
+          <button>start</button>
+        </form>
+      </div>
+
+      <div>
+        {timer && <Countdown />}
+      </div>
+
+      <div>
+        <h2>Timer history</h2>
+        items
+      </div>
     </div>
   );
 }
 
-export default App;
